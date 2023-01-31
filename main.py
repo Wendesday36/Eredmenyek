fd3bf7565fbc84788c59fe245f2b87530bf78a0c
Wendesday36 <bertazoeanna@gmail.com>
2023-01-31 10:55:47 +0100

    lapok osszege

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #megoldas
-def eredmeny(gepp,jatekosp):
-    if gepp > 21 :
+def eredmeny(jatekoskatya: [int], gepkartya : [int]):
+    if osszeg(jatekoskatya) > 21 :
         print("vesztett")
-    if jatekosp > 21:
+    if osszeg(gepkartya) > 21:
         print("vesztett")
 
     #tesztesetek
+
+def osszeg(kartyak: [int]):
+    pontok: int=0
+    for i in range(len(kartyak)):
+        pontok += kartyak[i]
+    return pontok
